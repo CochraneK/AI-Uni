9e523f76939f0f34df02857385f08cc577753f09
CochraneK <48022373+CochraneK@users.noreply.github.com>
2026-09-15 00:45:59 +0800

    chore: expose first-week day API to client types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -35,6 +35,7 @@ import type * as engine_abstractGame from "../engine/abstractGame.js";
 import type * as engine_historicalObject from "../engine/historicalObject.js";
 import type * as http from "../http.js";
 import type * as init from "../init.js";
+import type * as life_day from "../life/day.js";
 import type * as life_state from "../life/state.js";
 import type * as messages from "../messages.js";
 import type * as music from "../music.js";
@@ -89,6 +90,7 @@ declare const fullApi: ApiFromModules<{
   "engine/historicalObject": typeof engine_historicalObject;
   http: typeof http;
   init: typeof init;
+  "life/day": typeof life_day;
   "life/state": typeof life_state;
   messages: typeof messages;
   music: typeof music;
